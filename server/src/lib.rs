f3a33a39c9c1c4b3bdd54d158e3f76dcd075fab7
d34thm0u53 <d34thm0u53@gmail.com>
2025-11-29 12:59:49 +1000

    Don't init scheduled

--- server/src/lib.rs
+++ server/src/lib.rs
@@ -7,30 +7,15 @@ use modules::player::*;
 use modules::*;
 
 pub mod schedulers;
-use schedulers::*;
-
 
 
 #[reducer(init)]
 // Called when a client connects to a SpacetimeDB database server
 fn database_init(ctx: &ReducerContext) {
-    // scheduler_chunks::init(ctx)
-    //     .expect("Failed to initialize chunk scheduler");
-
-    scheduler_chat_archive::init(ctx)
-        .expect("Failed to initialize chat archive timer");
-    scheduler_chunks::init(ctx)
-        .expect("Failed to initialize chunk scheduler");
-
-    // Initialize the database
-    // Authentication is now handled by SpaceTimeAuth
-
     // Initialize global configuration
     util::init_default_configs(ctx)
         .expect("Failed to initialize global configuration");
 
-    // Initialize default weapons
-    // crate::modules::weapon::initialize_default_weapons(ctx);
 }
 
 #[reducer(client_connected)]
