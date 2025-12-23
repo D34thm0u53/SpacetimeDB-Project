ff954ab54726486a8f636d7f426e18e4502e5a54
d34thm0u53 <d34thm0u53@gmail.com>
2025-12-23 14:00:43 +1000

    MG-14: only allow spacetime auth connections

--- server/src/lib.rs
+++ server/src/lib.rs
@@ -16,10 +16,23 @@ fn database_init(ctx: &ReducerContext) {
         .expect("Failed to initialize global configuration");
 }
 
-/// Handles client connection events, creating or updating player accounts.
+
+// Set this to your the OIDC client (or set of clients) set up for your
+// SpacetimeAuth project.
+const OIDC_CLIENT_ID: &str = "client_031CVDRbDed69EKkv8duSe";
+
 #[reducer(client_connected)]
-fn client_connected(ctx: &ReducerContext) {
+pub fn client_connected(ctx: &ReducerContext) -> Result<(), String> {
+    let jwt = ctx.sender_auth().jwt().ok_or("Authentication required".to_string())?;
+    if jwt.issuer() != "https://auth.spacetimedb.com/oidc" {
+        return Err("Invalid issuer".to_string());
+    }
+
+    if !jwt.audience().iter().any(|a| a == OIDC_CLIENT_ID) {
+        return Err("Invalid audience".to_string());
+    }
     handle_player_connection_event(ctx, "connect");
+    Ok(())
 }
 
 /// Handles client disconnection events, moving players to offline status.
